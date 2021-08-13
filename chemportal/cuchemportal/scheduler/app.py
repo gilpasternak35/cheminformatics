481db927909fca6a0cccb728113a1477f2bea922
gil pasternak <gpasternak@nvidia.com>
2021-08-13 16:46:55 -0700

    Kubernetes configurations for pod and job added, Dockerfile modified

--- chemportal/cuchemportal/scheduler/app.py
+++ chemportal/cuchemportal/scheduler/app.py
@@ -1,21 +1,5 @@
-from cuchemportal.scheduler.workerB import trim_string
-from cuchemportal.scheduler.workerA import concatenate_strings
-from flask import (
-   Flask,
-   request,
-   jsonify,
-)
-@app.route("/add")
-def add():
-   first_str = request.args.get('first_num')
-   second_str = request.args.get('second_num')
-   result = concatenate_strings.delay(first_str, second_str )
-   return jsonify({'result': result}), 200
+def return_value(value: int, other: int) -> str:
+   """ Trivial script testing containerization"""
+   return str(value+other)
 
-
-@app.route("/subtract")
-def subtract():
-   first_str = request.args.get('first_num')
-   result = trim_string.delay(first_str)
-   return jsonify({'result': result}), 200
 
